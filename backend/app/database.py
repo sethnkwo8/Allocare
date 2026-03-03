4f85e4902d6121f9b6fbd1d9d2dd3d6446cb303c
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-03 16:08:42 +0100

    Created database connection and initial migration

---
+++ backend/app/database.py
@@ -0,0 +1,23 @@
+import os
+from sqlmodel import create_engine, Session
+from dotenv import load_dotenv
+from contextlib import contextmanager
+
+load_dotenv()
+
+# Fetch url
+postgres_url = os.getenv("DATABASE_URL")
+
+if not postgres_url:
+    raise ValueError("DATABASE_URL not found in .env file")
+
+is_dev = os.getenv("ENV", "development") == "development" # development is default
+engine = create_engine(postgres_url, echo=is_dev, connect_args={"options": "-c timezone=utc"}) # echo logs all generated SQL to terminal
+
+# Ensure sessions are safe in async
+@contextmanager
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
