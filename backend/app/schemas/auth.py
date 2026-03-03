fc5fb0a65a2ff0d609a05180acec8d4546be9cf7
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-03 19:26:33 +0100

    Update auth.py by adding RegisterResponse

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,4 +1,7 @@
 from pydantic import BaseModel, EmailStr, field_validator, Field, SecretStr
+import uuid
+from datetime import datetime
+
 
 # Schema for incoming registration payload
 class RegisterRequest(BaseModel):
@@ -9,3 +12,12 @@ class RegisterRequest(BaseModel):
     @classmethod
     def validate_email(cls, v: str) -> str:
         return v.lower().strip()
+
+# Schema for what is returned as a response
+class RegisterResponse(BaseModel):
+    id: uuid.UUID
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
