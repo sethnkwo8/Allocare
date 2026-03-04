2832447a5c823ed08d892d97ce617b103ca9b5a8
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-04 15:48:53 +0100

    Created base class for all errors

--- backend/app/auth/exception.py
+++ backend/app/auth/exception.py
@@ -1,6 +1,24 @@
-# Custom domain error
-class UserAlreadyExistsError(Exception):
-    def __init__(self, email: str):
-        self.email = email
-        self.message = f"User with email {email} already exists."
+# Base class for all auth errors
+class AuthError(Exception):
+    def __init__(self, message: str, status_code: int, code: str):
+        self.message = message
+        self.status_code = status_code
+        self.code = code
         super().__init__(self.message)
+
+class UserAlreadyExistsError(AuthError):
+    def __init__(self, email: str):
+        super().__init__(
+            message=f"User with email {email} already exists",
+            status_code=400,
+            code="USER_ALREADY_EXISTS"
+        )
+
+class InvalidCredentialsError(AuthError):
+    def __init__(self):
+        super().__init__(
+            message="Invalid email or password",
+            status_code=401,
+            code="INVALID_CREDENTIALS"
+        )
+
