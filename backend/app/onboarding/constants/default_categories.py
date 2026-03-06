1af8b429c4461dc32fb3118584205e342ff82896
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-06 17:25:01 +0100

    Create default_categories.py

---
+++ backend/app/onboarding/constants/default_categories.py
@@ -0,0 +1,18 @@
+DEFAULT_CATEGORIES = {
+    "needs": [
+        "Housing",
+        "Groceries",
+        "Utilities",
+        "Transport",
+        "Healthcare"
+    ],
+    "wants": [
+        "Dining Out",
+        "Entertainment",
+        "Shopping"
+    ],
+    "savings": [
+        "Emergency Fund",
+        "Investments"
+    ]
+}
