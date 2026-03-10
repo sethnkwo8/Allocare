efeafe0e378856af9f8ece952aba59bfec2e1a9f
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-10 15:28:00 +0100

    Updated create_notification helper function

--- backend/app/utils/create_notification.py
+++ backend/app/utils/create_notification.py
@@ -1,12 +1,21 @@
 from app.models.notification import Notification, NotificationType
 import uuid
+from typing import Optional
 
 # Helper function to create notification
-def create_notification(notification_type: NotificationType, message: str, user_id: uuid.UUID, db_session):
+def create_notification(title: str,
+                        notification_type: NotificationType,
+                        message: str,
+                        user_id: uuid.UUID,
+                        db_session,
+                        reference_id: Optional[uuid.UUID] = None
+                        ):
     notification = Notification(
+        title=title,
         type=notification_type,
         message=message,
-        user_id=user_id
+        user_id=user_id,
+        reference_id=reference_id
     )
 
     db_session.add(notification)
