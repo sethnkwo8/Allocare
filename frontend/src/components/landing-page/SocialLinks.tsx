9fea1305ce2fb10baf4d78fb805ccc082d508ff0
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-14 11:23:56 +0100

    Added aria-label to social links

--- frontend/src/components/landing-page/SocialLinks.tsx
+++ frontend/src/components/landing-page/SocialLinks.tsx
@@ -5,18 +5,21 @@ export function SocialLinks() {
     return (
         <div className="flex gap-4">
             <Link href="https://github.com/sethnkwo8"
+                aria-label='GitHub profile'
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-[#2E6B6B] text-gray-500 transition-colors">
                 <Github size={20} />
             </Link>
             <Link href="https://www.linkedin.com/in/seth-nkwo/"
+                aria-label='LinkedIn profile'
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-[#2E6B6B] text-gray-500 transition-colors">
                 <Linkedin size={20} />
             </Link>
             <Link href="mailto:sethnkwo@yahoo.com"
+                aria-label='Email'
                 target="_blank"
                 rel="noopener noreferrer"
                 className="hover:text-[#2E6B6B] text-gray-500 transition-colors">
