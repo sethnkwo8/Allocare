29586279a53dcf7a66c0d4da6926d064907106f3
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-22 19:24:56 +0100

    Update layout.tsx

--- frontend/src/app/(protected)/layout.tsx
+++ frontend/src/app/(protected)/layout.tsx
@@ -1,10 +1,13 @@
 import { AuthGuard } from "@/components/auth/AuthGuard"
 import { Metadata } from "next"
 
-// Metadata for auth routes
+// Metadata for protected routes
 export const metadata: Metadata = {
-    title: "Onboarding || Allocare",
-    description: "Set up your Allocare budget details.",
+    title: {
+        template: "%s || Allocare", // %s is used as a placeholder
+        default: "Allocare",
+    },
+    description: "Securely access your Allocare budget account.",
 }
 
 export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
