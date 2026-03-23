01ffe805df1cbc41fbbb54942775c08ea6f3a44c
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-23 16:19:10 +0100

    Dashboard Content

---
+++ frontend/src/components/dashboard/Dashboard.tsx
@@ -0,0 +1,29 @@
+// Dashboard
+"use client"
+
+import { useDashboard } from "@/hooks/useDashboard"
+import { OverviewCards } from "./OverviewCards"
+import { Header } from "./Header"
+
+export function Dashboard() {
+    const { data, isLoading, errorData, refresh } = useDashboard()
+
+    if (isLoading) return <div>Loading your financial overview...</div>
+    if (errorData) return <div>Error: {errorData}</div>
+    if (!data) return null
+
+    const incomeValue = data.financial_overview.total_income
+    const totalSpent = data.financial_overview.total_spent
+
+    return (
+        <div className="min-h-screen bg-linear-to-br from-[#d4f1f1] to-[#e6f5f5] p-4 md:p-8">
+            <div className="max-w-7xl mx-auto space-y-6">
+                {/* Header */}
+                <Header />
+
+                {/* Overview Cards */}
+                <OverviewCards data={data} />
+            </div>
+        </div>
+    )
+}
