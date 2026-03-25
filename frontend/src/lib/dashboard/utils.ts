0b029a751bd7814472420b164b6ba7ae13db03dd
Seth Nkwo <sethnkwocool@gmail.com>
2026-03-25 20:59:39 +0100

    Created util to format amount with commas

--- frontend/src/lib/dashboard/utils.ts
+++ frontend/src/lib/dashboard/utils.ts
@@ -4,3 +4,12 @@ import { currencies } from "../onboarding/currency";
 export function getCurrencySymbol(code: string) {
     return currencies.find(c => c.code === code)?.symbol || code;
 }
+
+// Format amount with currency
+export const formatWithCommas = (value: number | string) => {
+    const num = Number(value || 0);
+    return new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(num);
+};
