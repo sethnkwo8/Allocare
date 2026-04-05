a70f96b7c2da68d68ce5637dcb23310d119325db
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-05 17:21:29 +0100

    Created all expenses page

---
+++ frontend/src/app/(protected)/expenses/page.tsx
@@ -0,0 +1,11 @@
+// All expenses page
+import { AllExpenses } from "@/components/expenses/AllExpenses";
+
+// Metadata title
+export const metadata = { title: "All Expenses" };
+
+export default function AllExpensesPage() {
+    return (
+        <AllExpenses />
+    )
+}
