e18a553cb2d5c994ad19c1d42a2ee52c79ba7c00
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-07 22:43:38 +0100

    Added route for update buget allocations

--- backend/app/onboarding/router.py
+++ backend/app/onboarding/router.py
@@ -35,3 +35,12 @@ def complete_onboarding(
     service.complete_onboarding(session_token, db_session, payload)
 
     return {"message": "Onboarding complete"}
+
+# PATCH route to update budgets
+@router.patch("/budget/update", status_code=200)
+def update_allocations(
+    payload: schema.OnboardingRequest,
+    session_token: Annotated[Optional[str], Cookie()] = None,
+    db_session: Session = Depends(get_session)
+):
+    return service.update_budget_allocations(session_token=session_token, db_session=db_session, buckets_data=payload)
