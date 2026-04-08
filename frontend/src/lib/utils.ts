10e86db117ab4dfbc7bbda0098285cd9982a2e44
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-08 11:25:10 +0100

    Created util for formatting keys

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Helper to format keys in category settings
+export const formatLabel = (text: string) => {
+  const result = text.replace(/([A-Z])/g, " $1");
+  return result.charAt(0).toUpperCase() + result.slice(1);
+};
