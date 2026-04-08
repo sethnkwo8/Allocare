6fba49631588bf134db1cd9830587fbbaf931c85
Seth Nkwo <sethnkwocool@gmail.com>
2026-04-08 19:37:54 +0100

    Added metadata for profile settings page

--- frontend/src/app/(protected)/profile-settings/page.tsx
+++ frontend/src/app/(protected)/profile-settings/page.tsx
@@ -1,6 +1,10 @@
 import { ProfileSettings } from "@/components/profile-settings/ProfileSettings";
 
 // Profile settings page
+
+// Metadata title
+export const metadata = { title: "Profile Settings" };
+
 export default function ProfileSettingsPage() {
     return (
         <ProfileSettings />
